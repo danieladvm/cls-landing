b85cbf984715a20ac974ba6ae643a19170234a17
Daniela Mohamed <dmohamed@globons.com>
2019-08-01 23:20:26 -0300

    Updates

---
+++ precache-manifest.03d7bf13b21d4c8e2027d91b829f1aac.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7271e9d6c13ee8ab4ca6e398a65a7f9",
+    "url": "/cls-landing/index.html"
+  },
+  {
+    "revision": "a30a6b480a32152a66f2",
+    "url": "/cls-landing/static/css/2.9ba6e7c7.chunk.css"
+  },
+  {
+    "revision": "c364cfd8e163632ff434",
+    "url": "/cls-landing/static/css/main.12205e09.chunk.css"
+  },
+  {
+    "revision": "a30a6b480a32152a66f2",
+    "url": "/cls-landing/static/js/2.d77cedb4.chunk.js"
+  },
+  {
+    "revision": "c364cfd8e163632ff434",
+    "url": "/cls-landing/static/js/main.28050baf.chunk.js"
+  },
+  {
+    "revision": "1d9e8a22d5c871a7e639",
+    "url": "/cls-landing/static/js/runtime~main.4240c693.js"
+  },
+  {
+    "revision": "56259f39b0a915a18c2e18344e557da2",
+    "url": "/cls-landing/static/media/Grafica-Sedesss-18.56259f39.jpg"
+  },
+  {
+    "revision": "3d14a33b5a5641d364f4a0050b962531",
+    "url": "/cls-landing/static/media/canuelas.3d14a33b.jpg"
+  },
+  {
+    "revision": "9b4551bfef9808cac1809fe0750eadda",
+    "url": "/cls-landing/static/media/carolina.9b4551bf.jpg"
+  },
+  {
+    "revision": "060ea8dd8446c15159ed6ec217ae29b9",
+    "url": "/cls-landing/static/media/fa-brands-400.060ea8dd.svg"
+  },
+  {
+    "revision": "4f786efdf6328877ec2448bf265bcc8f",
+    "url": "/cls-landing/static/media/fa-brands-400.4f786efd.woff2"
+  },
+  {
+    "revision": "72f5c754189a296467db59e26e2b4671",
+    "url": "/cls-landing/static/media/fa-brands-400.72f5c754.woff"
+  },
+  {
+    "revision": "75dae2b62813bc641a9b6a2fc041e4de",
+    "url": "/cls-landing/static/media/fa-brands-400.75dae2b6.ttf"
+  },
+  {
+    "revision": "e33a1a54eb0a1e560362fcf5335ac26c",
+    "url": "/cls-landing/static/media/fa-brands-400.e33a1a54.eot"
+  },
+  {
+    "revision": "0234a585809be071bd68db999f27f829",
+    "url": "/cls-landing/static/media/fa-regular-400.0234a585.svg"
+  },
+  {
+    "revision": "76d069c84f54a63d1bf9e29c76fe65e6",
+    "url": "/cls-landing/static/media/fa-regular-400.76d069c8.ttf"
+  },
+  {
+    "revision": "d90e1b03a9168c0d775a52098423adfb",
+    "url": "/cls-landing/static/media/fa-regular-400.d90e1b03.woff2"
+  },
+  {
+    "revision": "d98ffd73c6b104b42bfe69c987a72924",
+    "url": "/cls-landing/static/media/fa-regular-400.d98ffd73.eot"
+  },
+  {
+    "revision": "f9695ea8029b2975dea6753bf6f1bf1c",
+    "url": "/cls-landing/static/media/fa-regular-400.f9695ea8.woff"
+  },
+  {
+    "revision": "55ce8d0ca92adb7c283d6043d0edf46f",
+    "url": "/cls-landing/static/media/fa-solid-900.55ce8d0c.woff"
+  },
+  {
+    "revision": "60ce8cf4dd9fe177abdfeda21e20798e",
+    "url": "/cls-landing/static/media/fa-solid-900.60ce8cf4.woff2"
+  },
+  {
+    "revision": "d42cc8bb088b9bbcb51b1db2d9210976",
+    "url": "/cls-landing/static/media/fa-solid-900.d42cc8bb.svg"
+  },
+  {
+    "revision": "e9809ab6a5a34dc6b7966de82265d6bd",
+    "url": "/cls-landing/static/media/fa-solid-900.e9809ab6.ttf"
+  },
+  {
+    "revision": "f9b8a1aa417e318d5be775e14ee3c475",
+    "url": "/cls-landing/static/media/fa-solid-900.f9b8a1aa.eot"
+  },
+  {
+    "revision": "22602db8aec33ba9f6a86a2f3662e7ac",
+    "url": "/cls-landing/static/media/lafe.22602db8.jpg"
+  },
+  {
+    "revision": "1a8d8f0cc7f6c79208db603ec7fb3b53",
+    "url": "/cls-landing/static/media/logo-cls.1a8d8f0c.png"
+  },
+  {
+    "revision": "7c94876cd22ca992df333d928a9981f4",
+    "url": "/cls-landing/static/media/lomas-z.7c94876c.jpg"
+  },
+  {
+    "revision": "eb6dc47dd862e9a5e5e88862612d9928",
+    "url": "/cls-landing/static/media/ramos.eb6dc47d.jpg"
+  },
+  {
+    "revision": "ba1906e11fbb2b82ee00e6625154c783",
+    "url": "/cls-landing/static/media/reunion-general.ba1906e1.jpg"
+  }
+]);
